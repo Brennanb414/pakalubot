3349c569a0c94f3d5c39ef6ff90fa5325a8626e5
Brennanb414 <iceiscold86@aim.com>
2016-11-06 10:48:25 -0600

    cooler name for title

--- redditbot.py
+++ redditbot.py
@@ -4,18 +4,17 @@ import OAuth2Util
 from credentials import *
 
 r = praw.Reddit('pakalu twitter -> reddit ver 0.1'
-		'URL: github.com/Brennanb414/pakalubot')
+		'URL: github.com/brennanb414/pakalubot')
 o = OAuth2Util.OAuth2Util(r)
 o.refresh(force=True)
 
 def PostTweet(text,user,tweetID):
-	subreddit = r.get_subreddit("pakalupapito")
-	title = text
+	subreddit = r.get_subreddit("oneoneoneoneone")
+	title = "pakalu: "+text
 	twitterURL = 'https://twitter.com/'+str(user)+'/status/'+str(tweetID)
 	
 	try:
 		subreddit.submit(title,url=twitterURL)
-		print("posted"+title)
+		print("posted "+title+" to "+str(subreddit))
 	except praw.errors.APIException:
 		print("submission failed")	
-
