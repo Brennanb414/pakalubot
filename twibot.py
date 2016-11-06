b81256d34d0bf64a43ffd74a3c23d8fc1f4612da
Brennanb414 <iceiscold86@aim.com>
2016-11-06 10:47:28 -0600

    error handling added to tweet stream part

--- twibot.py
+++ twibot.py
@@ -1,10 +1,13 @@
 #Original author: github.com/Brennanb414
+#
 #requires:
 #	-tweepy 3.3.0
 #	-praw
 #	-OAuth2Util
 #
 
+import logging
+import traceback
 from credentials import *
 from tweepy import StreamListener
 from tweepy import Stream
@@ -22,20 +25,25 @@ api = tweepy.API(auth)
 class MyStreamListener(StreamListener):
 	
     def on_status(self, status):
-        # called when user tweets
-	print("\n------Tweet info-------"
-		"\nAuthor: "+str(status.author.screen_name)+
-		"\nID: "+str(status.id)+
-		"\nText: "+str(status.text))
+        # called when user tweets, this makes it so only pakalu's tweets get posted, and not all the RTs
 	if(status.author.screen_name == "pakalupapito"):
-		PostTweet(status.text,status.author.screen_name,status.id)
-
+		try:		
+			#raise Exception("hey, shit's fucked")
+			print("\n------Tweet info-------"
+			"\nAuthor: "+str(status.author.screen_name)+
+			"\nID: "+str(status.id)+
+			"\nText: "+str(status.text))
+			#PostTweet(status.text,status.author.screen_name,status.id)
+		except Exception as e:
+			print("Tweet error: "+str(traceback.format_exc)+ "\n E: "+e)			
+			
     def on_error(self, status):
 	if status == 420:
 		print ("Error was 420, waiting 15 mins")
 		time.sleep(60*15)
+		print("\nwait over")
 	else:
-		print(status)
+		print("Error was: "+str(status))
 
 if __name__ == '__main__':
 	listener = MyStreamListener()
@@ -43,3 +51,4 @@ if __name__ == '__main__':
 
 	#must be the user's ID. get it from here: https://tweeterid.com/
 	twitterStream.filter(follow=['1582341876'])
+
